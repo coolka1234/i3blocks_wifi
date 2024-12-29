123cc036ff7d7c78e14d639cc53db285807a7053
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-12-29 14:39:58 +0100

    final version

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::process::{Command, Output, Stdio};
+use colored::Colorize;
 
 fn main(){
     let wifi_level = get_wifi_level();
     let wifi_string_level = match wifi_level {
-        0..=30 => "None",
-        31..=53 => "Low",
-        54..=70 => "Medium",
-        71..=100 => "High",
-        _ => "None",
+        1..=30 => "None".blue(),
+        31..=53 => "Low".yellow(),
+        54..=70 => "Medium".yellow().green(),
+        71..=100 => "High".green(),
+        _ => "None".white(),
     };
-    const WIFI_SYMBOL: &str= "📶";
-    println!("Wifi level: {}", WIFI_SYMBOL.to_owned()+&wifi_string_level);
+    println!("Wifi level: 📶 {}", wifi_string_level);
 }
 
 fn get_wifi_level() -> i32 {
@@ -27,14 +27,15 @@ fn get_wifi_level() -> i32 {
         .spawn()
         .unwrap();
     let Output { stdout ,..} = awk.wait_with_output().unwrap();
-    //this may be empty btw if there is now wifi
     let result = String::from_utf8_lossy(&stdout);
 
     let cleaned_result= result.trim_end_matches(".\n");
     if cleaned_result.is_empty() {
         return -1;
     }
-
-    let output = cleaned_result.parse::<i32>().unwrap();
+    let output = match cleaned_result.parse::<i32>()  {
+        Ok(value) => value,
+        Err(_) => -1,
+    };
     output
 }
